0f1b0e367104037366b4564c39ff9ad57bc7ff93
Aditya Baisakh <adityabaisakh123@gmail.com>
2026-07-04 10:56:19 -0500

    feat: persist theme preference

--- main.js
+++ main.js
@@ -1,5 +1,18 @@
 const themeToggle = document.getElementById("#theme-toggle");
+const savedTheme = localStorage.getItem("theme");
+
+if (savedTheme === "dark") {
+    document.body.classList.add("dark-mode");
+}
 
 themeToggle.addEventListener("click", () => {
     document.body.classList.toggle("dark-mode");
+
+    const isDarkMode = document.body.classList.contains("dark-mode");
+
+    if (isDarkMode) {
+        localStorage.setItem("theme", "dark");
+    } else {
+        localStorage.setItem("theme", "light");
+    }
 });
